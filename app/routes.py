5383e1dc7e7b10adb55f703bca3c021d81596583
shallon <shallonmokeira@gmail.com>
2024-11-18 00:49:31 +0300

    protect api endpoints using decorators

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,19 @@
 from flask import Blueprint, request, jsonify
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from models import db, Customer, Order
 from datetime import datetime
 
 api = Blueprint('api', __name__)
 
 @api.route('/customers', methods=['POST'])
+@jwt_required()  # Protect this endpoint with JWT
 def add_customer():
+    """
+    Endpoint to add a new customer.
+    Only accessible by authenticated users.
+    """
+    current_user = get_jwt_identity()  # Get the identity of the current user
+    
     data = request.json
     customer = Customer(name=data['name'], code=data['code'])
     db.session.add(customer)
@@ -13,7 +21,9 @@ def add_customer():
     return jsonify({'message': 'Customer added'}), 201
 
 @api.route('/orders', methods=['POST'])
+@jwt_required()  # Protect this endpoint with JWT
 def add_order():
+    current_user = get_jwt_identity()
     data = request.json
     order = Order(item=data['item'], amount=data['amount'], time=datetime.utcnow(), customer_id=data['customer_id'])
     db.session.add(order)
