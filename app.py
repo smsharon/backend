ba521c3e42daf65f266db16531e49d7d035d6ee2
shallon <shallonmokeira@gmail.com>
2024-12-09 21:58:10 +0300

    updates

--- app.py
+++ app.py
@@ -34,6 +34,5 @@ def create_app(config_class=DevelopmentConfig):
 if __name__ == "__main__":
     app = create_app()
     with app.app_context():
-        # Create all tables
         db.create_all()
     app.run(debug=True)
